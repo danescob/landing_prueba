8b5de8bfc09f5174582430c28e0ce6a7e06f3ac5
Daniel <descobars@neverbit.com>
2019-04-15 03:02:07 -0500

    Envio por medio de servicio

--- src/web/src/app/component/formulario/formulario.component.ts
+++ src/web/src/app/component/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Envio } from '../../service/envio';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -7,12 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private envio: Envio,
+  ) { }
 
   ngOnInit() {
   }
 
-  enviar(componente: any){
+  enviar() {
+    if (this.form.valid) {
+      this.envio.create(this.form.value)
+        .subscribe((response: any) => {
+          if (this.result) {
+            alert('Gracias!!!');
+          } else {
+            alerts('Error al enviar la informacion');
+          }
+        }, error => {
+        });
+    } else {
+      this.markFormGroupTouched(this.form);
+    }
 
   }
 
--- src/web/src/app/service/envio.service.ts
+++ src/web/src/app/service/envio.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvioService {
 
-  constructor() { }
+  URL_BASE: string = `${environment.API_URL}/enviar`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(formulario: any) {
+
+    return this.http.post(this.URL_BASE, formulario)
+      .pipe(map( response => {
+        return response;
+      } ));
+
+  }
 }
